d132ac6f7aa5842468f415350fbd4ebf75354a8f
chtvrg <kristinachetverg@gmail.com>
2024-09-09 22:37:09 +0700

    jkj

---
+++ app/src/main/java/com/example/chetvergovapr22103kpz1/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.chetvergovapr22103kpz1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.chetvergovapr22103kpz1.ui.theme.ChetvergovaPR22103kpz1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ChetvergovaPR22103kpz1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Kristina")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello, my name is $name!",
+        modifier = modifier.padding(16.dp),
+        fontSize = 30.sp
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ChetvergovaPR22103kpz1Theme {
+        Greeting("Kristina")
+    }
+}
